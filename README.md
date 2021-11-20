8bf47e32e32dd86c9beac13ecca7495cbc9a7d70
mtrucc <mtrucc@qq.com>
2021-11-20 15:38:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-ygdu6n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-ygdu6n)
---
+++ src/icon/amazon.js
@@ -0,0 +1,8 @@
+import { createSvgIcon } from '@mui/material/utils';
+
+export const Amazon = createSvgIcon(
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+xxxx
+  </svg>,
+  'Amazon'
+);
